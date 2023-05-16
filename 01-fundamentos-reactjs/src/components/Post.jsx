8de0d64975165660bfdc67b80d6da1e9ef8c2f17
Breno Fischer <breno_perricone@hotmai.com>
2023-05-15 21:09:03 -0300

    project 1 starting point

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
+                        className={styles.avatar}
+                    />
+
+                    <div className={styles.authorInfo}>
+                        <strong>Breno Perricone</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='15 May 2023 08:19:00' dateTime='2023-05-15 08:19:00'>Published 1h ago</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a> {' '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Leave your feedback</strong>
+
+                <textarea placeholder='Leave a comment'/>
+                <footer>
+                    <button type='submit'>Comment</button>
+                </footer>
+            </form>
+        </article>
+    );
+}
