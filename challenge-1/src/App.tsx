3ee465e08bc75ed4a88cacba026cea2a7d21975a
Breno Fischer <breno_perricone@hotmai.com>
2023-05-18 19:50:29 -0300

    add 'new task' bar (structure + style)

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -1,10 +1,26 @@
-import { Header } from "./components/Header"
+import { PlusCircle } from '@phosphor-icons/react';
+
+import { Header } from "./components/Header";
+
+import styles from './App.module.css';
 
 function App() {
 
   return (
     <>
       <Header />
+      
+      <main className={styles.todoWrapper}>
+        <div className={styles.todoContent}>
+          <form className={styles.newTask}>
+            <input 
+              placeholder="Add a new task"
+            />
+
+            <button type="submit">Create <span><PlusCircle /></span></button>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
