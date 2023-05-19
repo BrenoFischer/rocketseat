5f1514e21794da373ccfba89202aa793c339d5c4
Breno Fischer <breno_perricone@hotmai.com>
2023-05-19 14:38:11 -0300

    add task structure + style

--- challenge-1/src/components/Task.tsx
+++ challenge-1/src/components/Task.tsx
@@ -5,7 +5,10 @@ import styles from './Task.module.css';
 export function Task() {
     return (
         <li className={styles.task}>
-            <input type='checkbox' />
+            <div className={styles.inputWrapper}>
+                <input type='checkbox' id='checkbox' />
+                <label htmlFor='checkbox'></label>
+            </div>
             <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
             <Trash size={20} />
         </li>
