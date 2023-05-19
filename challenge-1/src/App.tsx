6c139f4b0518c743f0a4a13499d78608f7defe8f
Breno Fischer <breno_perricone@hotmai.com>
2023-05-19 18:58:57 -0300

    start create task functionality

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -1,12 +1,28 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+
 import { PlusCircle } from '@phosphor-icons/react';
 
-import { Header } from "./components/Header";
 import Clipboard from './assets/clipboard.svg';
+import { Header } from "./components/Header";
+import { Task, TaskType } from './components/Task';
 
 import styles from './App.module.css';
-import { Task } from './components/Task';
 
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [newTaskContent, setNewTaskContent] = useState('');
+
+  function handleNewTaskContentChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskContent(event.target.value);
+  }
+
+  function handleNewTask(event: FormEvent) {
+    event.preventDefault();
+    const newTask = { content: newTaskContent, isCompleted: false }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskContent('');
+  }
 
   return (
     <>
@@ -14,9 +30,13 @@ function App() {
       
       <div className={styles.todoWrapper}>
         <div className={styles.todoContent}>
-          <form className={styles.newTask}>
+          <form onSubmit={handleNewTask} className={styles.newTask}>
             <input 
               placeholder="Add a new task"
+              value={newTaskContent}
+              name="newTask"
+              onChange={handleNewTaskContentChange}
+              required
             />
 
             <button type="submit">Create <span><PlusCircle /></span></button>
@@ -33,16 +53,19 @@ function App() {
                 <span className={styles.counter}>0</span>
               </div>
             </header>
-            {/* <section className={styles.empty}>
-              <img src={Clipboard} />
-              <div>
-                <p><span>You still do not have any tasks created</span></p>
-                <p>Create tasks and organize your ToDos</p>
-              </div>
-            </section> */}
-
-            <Task />
-            <Task />
+            { tasks.length === 0 ?
+              <section className={styles.empty}>
+                <img src={Clipboard} />
+                <div>
+                  <p><span>You still do not have any tasks created</span></p>
+                  <p>Create tasks and organize your ToDos</p>
+                </div>
+              </section>
+              :
+              tasks.map(task => {
+                return <Task content={task.content} isCompleted={task.isCompleted} />
+              })
+            }
           </main>
         </div>
       </div>
--- challenge-1/src/components/Task.tsx
+++ challenge-1/src/components/Task.tsx
@@ -2,14 +2,19 @@ import { Trash } from '@phosphor-icons/react';
 
 import styles from './Task.module.css';
 
-export function Task() {
+export interface TaskType {
+    content: string;
+    isCompleted: boolean;
+}
+
+export function Task({ content, isCompleted }:TaskType) {
     return (
         <li className={styles.task}>
             <div className={styles.inputWrapper}>
                 <input type='checkbox' id='checkbox' />
                 <label htmlFor='checkbox'></label>
             </div>
-            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+            <p>{content}</p>
             <Trash size={20} />
         </li>
     );
