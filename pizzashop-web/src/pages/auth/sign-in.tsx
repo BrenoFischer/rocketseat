a757d99693f1a19f8b2214fd790acdc10c774d6d
Breno Fischer <breno_perricone@hotmail.com>
2024-07-07 18:04:51 -0300

    add auth and orders page

--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,59 @@
 import { Helmet } from "react-helmet-async"
+import { useForm } from 'react-hook-form'
+import { Link } from "react-router-dom"
+import { z } from 'zod'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { toast } from "sonner"
+
+const signInForm = z.object({
+    email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+
+    async function handleSignIn(data: SignInForm) {
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+            toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () => handleSignIn(data)
+                }
+            })
+        } catch {
+            toast.error('E-mail inválido.')
+        }
+    }
+
     return (
         <>
             <Helmet title="Login" />
-            <h1>Aith</h1>
+            <div className="p-8">
+                <Button variant="ghost" asChild className="absolute right-8 top-8">
+                    <Link className="" to="/sign-up">Novo estabelecimento</Link>
+                </Button>
+
+                <div className="w-[350px] flex flex-col justify-center gap-6">
+                    <div className="flex flex-col gap-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+                        <p className="text-sm text-muted-foreground">Acompanhar as vendas pelo painel do parceiro!</p>
+                    </div>
+
+                    <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Seu e-mail</Label>
+                            <Input id="email" type="email" {...register('email')} />
+                        </div>
+                        <Button disabled={isSubmitting} type="submit" className="w-full">Acessar painel</Button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 
 export const router = createBrowserRouter([
@@ -10,14 +13,16 @@ export const router = createBrowserRouter([
         element: <AppLayout />,
         children: [
             { path: '/', element: <Dashboard /> },
+            { path: '/orders', element: <Orders /> },
         ]
     },
 
     {
         path: '/',
-        element: <AppLayout />,
+        element: <AuthLayout />,
         children: [
             { path: '/sign-in', element: <SignIn /> },
+            { path: '/sign-up', element: <SignUp /> },
         ]
     }
 ])
